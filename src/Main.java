24b292e27f2b8e0478ff6acd1f97f82aa76eb013
cH <asas536112@gmail.com>
2017-09-18 15:06:46 +0800

    fin HW

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
-	        System.out.print("My First Java Code1");
+	        System.out.println("學號：105022079");
+            System.out.println("系級：行動商務與多媒體應用學系三年級");
+            System.out.println("姓名：陳和諺");
+            System.out.println("時間：2017/09/18");
+            System.out.println("地點：\"I319\"");
     }
 }
